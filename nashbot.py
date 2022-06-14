a04aa4cf0a8002a8ba136432f291819b2f8382db
2517003w <nwaugh444@gmail.com>
2022-06-14 12:23:05 +0100

    hi response
    randomises a response message to users saying hi
    
    also added an error log for unhandled exceptions

--- nashbot.py
+++ nashbot.py
@@ -4,6 +4,8 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import random
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -23,5 +25,36 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    hi_quotes = [
+        'hi??',
+        'u called?',
+        'mmm im responding',
+        'joe mama OOOOOHHHHHH-',
+        'this message is better than 6/10 hi response options',
+        'howdus :sunflower:',
+        'hello. but at what cost...',
+        'hi urself bucko',
+        '...are u talking... to me? h-hello?',
+        'greetings mortal',
+    ]
+
+    if message.content == 'hi':
+        response = random.choice(hi_quotes)
+        await message.channel.send(response)
+
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n\n')
+        else:
+            raise
 
 client.run(TOKEN)

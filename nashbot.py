e1a357b268ce8b307eef624b9355f283645ff497
2517003w <nwaugh444@gmail.com>
2022-06-14 11:59:14 +0100

    bot welcomes via dm
    sends new members a greeting dm on server join

--- nashbot.py
+++ nashbot.py
@@ -10,15 +10,18 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{client.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
     )
+
 
 client.run(TOKEN)

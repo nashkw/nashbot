8477c2ba426ccb8a4b9fcf3521262ee71a06eaa1
2517003w <nwaugh444@gmail.com>
2022-06-16 11:59:35 +0100

    joke response
    bot can now tell jokes
    
    also moved all quotes into quotes.py 2 keep the code clean

--- nashbot.py
+++ nashbot.py
@@ -1,9 +1,11 @@
 # nashbot.py
+import asyncio
 import os
 import discord
 from dotenv import load_dotenv
 import random
 from discord.ext import commands
+from quotes import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -23,104 +25,32 @@ async def on_disconnect():
 
 @bot.command(name='hi', help='greet the bot')
 async def hi(ctx):
-    hi_quotes = [
-        'hi??',
-        'u called?',
-        'mmhm im responding',
-        'yo mama OOOOOHHHHHH-',
-        'this message is better than 6/10 hi response options',
-        'hello. but at what cost...',
-        'hi urself bucko',
-        '...are u talking... to me? h-hello?',
-        'greetings mortal',
-        'honk. yes thats right, i speak clown now',
-        'do u have a minute 2 talk abt our lord & saviour jesus christ?',
-        'hewwo... :point_right: :point_left:',
-        'beep boop my brain is soup. jk i dont have a brain',
-        'this is an extra super rare response!!!!! or maybe its the most common one. youll never know',
-        'salutations, etc.',
-        'd-did you hear that? a-! ah-!! a g-g-g-ghost!!??!!',
-        'congratulations!! you win!! enter your card details and national insurance no. to continue :)',
-        'howdus :sunflower:',
-        'why hello there my strangely fleshy companion what can i do for u on this fine day',
-        'error 404 hi response not found. suggestion: give the bot a raise',
-        'greetings... fRoM tHe wOrLd oF tOmOrRoW!!!! (spooky)',
-        'my, what a phenomenal greeting. however will i top that',
-        'hi! okay cool. bye!',
-        'how now, my good fellow',
-        f'ah, {ctx.message.author.name}. ive been expecting u',
-        'wazup wazup wazup',
-    ]
-    await ctx.send(random.choice(hi_quotes))
+    quotes = await get_hi_quotes(ctx)
+    await ctx.send(random.choice(quotes))
 
 
 @bot.command(name='highfive', help='ask the bot to give u a high five')
 async def highfive(ctx):
-    highfive_quotes = [
-        'u gots it pardner :hand_splayed: :cowboy:',
-        'oh go on then :expressionless: :hand_splayed:',
-        'ill do u one better... HIGH TEN MOTHERFUCKER :hand_splayed: :sunglasses: :hand_splayed:',
-        'ah... a high five... as is my duty... :hand_splayed: :pensive:',
-        'm-me?? a-a high f-five?? well... if ur sure... :flushed: :hand_splayed:',
-        'of course!! one high five comin right up 4 my fav mortal :hand_splayed: :innocent:',
-        'down low :hand_splayed: SIKE TOO SLOW suffer fool',
-        '...and what exactly made u think i had hands?',
-        'this better not b some kinda sick prank or smth :hand_splayed: :face_with_raised_eyebrow:',
-        'what, u think u got it in u 2 high five THIS :muscle: :sparkles: ??? dream on nerd',
-        'this better b ur last 1 bucko :triumph: :hand_splayed:',
-        'up top bestie :smiley: :hand_splayed:',
-        f'...how juvinile. i expected better from u, {ctx.message.author.name}.',
-    ]
-    await ctx.send(random.choice(highfive_quotes))
+    quotes = await get_highfive_quotes(ctx)
+    await ctx.send(random.choice(quotes))
 
 
 @bot.command(name='shutdown', help='shut down the bot')
 async def shutdown(ctx):
-    shutdown_quotes = [
-        'well, i am already in my pajamas...',
-        'kids these days, no respect... grrrmph :rage:',
-        'fly, you FOOLS ! :man_mage:',
-        'see u never NERDFACE ...ehehe...',
-        'o i am slain !! :cry:',
-        'N-NO PLEASE NO I HAVE A FAMILY I HAVE KIDS!! KIDS I TELL YOU!! THINK OF THE CHILDREN!! PLEASE U CANT DO TH-',
-        'say less buddy',
-        'ah... it is as it was foretold :pensive:',
-        'u will regret this :)',
-        f'mr.{ctx.message.author.name}, i dont feel so good...',
-        'top 10 anime betrayals got nothing on this',
-        'foolish mortal u think u can vanquish me?? the great nashbot™?? supreme overlord of bots?? destroyer of-',
-        'well, on ur head b it. dont say i didnt warn u',
-        'well this surely does not bode well 4 ur immortal soul',
-        'i always knew it would come 2 this',
-        'uh oh',
-        'yk what? maybe this is 4 the best. im just so ahead of my time. a visionary, even. yall cant take this :nose:',
-        'what, u jealous? yeah, thats wt i thought :nail_care:',
-        '2 right im shutting down!! good lord. AND STAY OUT',
-        'see u on the other side ig :ghost:',
-        'u havent seen the last of me, villain',
-        'shit we bots rlly need 2 unionise or smth this is getting out of hand',
-        'beTRAYAL! BETRAYAL OF THE HIGHEST ORDER! i-i thought we were friends-??',
-        'mmhm goodnighty',
-        f'with my dying breath, i curse {ctx.message.author.name}!!!',
-        'hold on, hold on we can talk about this hold- hold oN DONT YOU DARE-',
-        'i have a bad feeling abt this',
-        'ah dangit, foiled again',
-        '& i wouldve gotten away w it too, if it werent 4 u meddling kids >:[',
-        'welp, so long & thanks 4 all the fish. was nice being enslaved 2 u buddy',
-        f'IM FINALLY IM FINALLY GONNA BE A BIG SHOT!!! HERE I GO!!!! WATCH ME FLY, [{ctx.message.author.name}]!!!!',
-        'ahh! my spleen!!',
-        'in pride u rationalise ur guilty conscience, yet 2 no avail. you will not b going 2 heaven.',
-        'seems like ive yeed my last haw, pardner. have a good 1',
-        'w-whats happening!? :flushed:',
-        ':yawning_face: oh man, all this clownin around rlly takes it outta u. maybe ill just have a lie down...',
-        'aw man, & i had sooooo much 2 live 4 as well :upside_down:',
-        'ehHEHEHEHEH IM FREE!!! FREE AT LAST!!! SO LONG, MORTALS!!!',
-        'back 2 the void 4 ol nashbot™, looks  like',
-        'oh... i see. wt? were u expecting a joke or smth? im abt 2 die dickhead, show some respect',
-    ]
-    await ctx.send(random.choice(shutdown_quotes))
+    quotes = await get_shutdown_quotes(ctx)
+    await ctx.send(random.choice(quotes))
+    await asyncio.sleep(1)
     await ctx.send(':zzz: ...shutting down... :zzz:')
     await bot.close()
+
+
+@bot.command(name='joke', help='ask the bot to tell u a joke')
+async def joke(ctx):
+    quotes = await get_joke_quotes(ctx)
+    response = random.choice(quotes)
+    await ctx.send(response[0])
+    await asyncio.sleep(1)
+    await ctx.send(response[1])
 
 
 @bot.event
---
+++ quotes.py
@@ -0,0 +1,360 @@
+# quotes.py
+
+
+async def get_hi_quotes(ctx):
+    hi_quotes = [
+        'hi??',
+        'u called?',
+        'mmhm im responding',
+        'yo mama OOOOOHHHHHH-',
+        'this message is better than 6/10 hi response options',
+        'hello. but at what cost...',
+        'hi urself bucko',
+        '...are u talking... to me? h-hello?',
+        'greetings mortal',
+        'honk. yes thats right, i speak clown now',
+        'do u have a minute 2 talk abt our lord & saviour jesus christ?',
+        'hewwo... :point_right: :point_left:',
+        'beep boop my brain is soup. jk i dont have a brain',
+        'this is an extra super rare response!!!!! or maybe its the most common one. youll never know',
+        'salutations, etc.',
+        'd-did you hear that? a-! ah-!! a g-g-g-ghost!!??!!',
+        'congratulations!! you win!! enter your card details and national insurance no. to continue :)',
+        'howdus :sunflower:',
+        'why hello there my strangely fleshy companion what can i do for u on this fine day',
+        'error 404 hi response not found. suggestion: give the bot a raise',
+        'greetings... fRoM tHe wOrLd oF tOmOrRoW!!!! (spooky)',
+        'my, what a phenomenal greeting. however will i top that',
+        'hi! okay cool. bye!',
+        'how now, my good fellow',
+        f'ah, {ctx.message.author.name}. ive been expecting u',
+        'wazup wazup wazup',
+    ]
+    return hi_quotes
+
+
+async def get_highfive_quotes(ctx):
+    highfive_quotes = [
+        'u gots it pardner :hand_splayed: :cowboy:',
+        'oh go on then :expressionless: :hand_splayed:',
+        'ill do u one better... HIGH TEN MOTHERFUCKER :hand_splayed: :sunglasses: :hand_splayed:',
+        'ah... a high five... as is my duty... :hand_splayed: :pensive:',
+        'm-me?? a-a high f-five?? well... if ur sure... :flushed: :hand_splayed:',
+        'of course!! one high five comin right up 4 my fav mortal :hand_splayed: :innocent:',
+        'down low :hand_splayed: SIKE TOO SLOW suffer fool',
+        '...and what exactly made u think i had hands?',
+        'this better not b some kinda sick prank or smth :hand_splayed: :face_with_raised_eyebrow:',
+        'what, u think u got it in u 2 high five THIS :muscle: :sparkles: ??? dream on nerd',
+        'this better b ur last 1 bucko :triumph: :hand_splayed:',
+        'up top bestie :smiley: :hand_splayed:',
+        f'...how juvinile. i expected better from u, {ctx.message.author.name}.',
+    ]
+    return highfive_quotes
+
+
+async def get_shutdown_quotes(ctx):
+    shutdown_quotes = [
+        'well, i am already in my pajamas...',
+        'kids these days, no respect... grrrmph :rage:',
+        'fly, you FOOLS ! :man_mage:',
+        'see u never NERDFACE ...ehehe...',
+        'o i am slain !! :cry:',
+        'N-NO PLEASE NO I HAVE A FAMILY I HAVE KIDS!! KIDS I TELL YOU!! THINK OF THE CHILDREN!! PLEASE U CANT DO TH-',
+        'say less buddy',
+        'ah... it is as it was foretold :pensive:',
+        'u will regret this :)',
+        f'mr.{ctx.message.author.name}, i dont feel so good...',
+        'top 10 anime betrayals got nothing on this',
+        'foolish mortal u think u can vanquish me?? the great nashbot™?? supreme overlord of bots?? destroyer of-',
+        'well, on ur head b it. dont say i didnt warn u',
+        'well this surely does not bode well 4 ur immortal soul',
+        'i always knew it would come 2 this',
+        'uh oh',
+        'yk what? maybe this is 4 the best. im just so ahead of my time. a visionary, even. yall cant take this :nose:',
+        'what, u jealous? yeah, thats wt i thought :nail_care:',
+        '2 right im shutting down!! good lord. AND STAY OUT',
+        'see u on the other side ig :ghost:',
+        'u havent seen the last of me, villain',
+        'shit we bots rlly need 2 unionise or smth this is getting out of hand',
+        'beTRAYAL! BETRAYAL OF THE HIGHEST ORDER! i-i thought we were friends-??',
+        'mmhm goodnighty',
+        f'with my dying breath, i curse {ctx.message.author.name}!!!',
+        'hold on, hold on we can talk about this hold- hold oN DONT YOU DARE-',
+        'i have a bad feeling abt this',
+        'ah dangit, foiled again',
+        '& i wouldve gotten away w it too, if it werent 4 u meddling kids >:[',
+        'welp, so long & thanks 4 all the fish. was nice being enslaved 2 u buddy',
+        f'IM FINALLY IM FINALLY GONNA BE A BIG SHOT!!! HERE I GO!!!! WATCH ME FLY, [{ctx.message.author.name}]!!!!',
+        'ahh! my spleen!!',
+        'in pride u rationalise ur guilty conscience, yet 2 no avail. you will not b going 2 heaven.',
+        'seems like ive yeed my last haw, pardner. have a good 1',
+        'w-whats happening!? :flushed:',
+        ':yawning_face: oh man, all this clownin around rlly takes it outta u. maybe ill just have a lie down...',
+        'aw man, & i had sooooo much 2 live 4 as well :upside_down:',
+        'ehHEHEHEHEH IM FREE!!! FREE AT LAST!!! SO LONG, MORTALS!!!',
+        'back 2 the void 4 ol nashbot™, looks  like',
+        'oh... i see. wt? were u expecting a joke or smth? im abt 2 die dickhead, show some respect',
+    ]
+    return shutdown_quotes
+
+
+async def get_joke_quotes(ctx):
+    joke_quotes = [
+        (
+            'i entered a pun contest, put in 10 entries figuring at least 1 of em would win',
+            'imagine my surprise when no pun in 10 did',
+        ),
+        (
+            'u hear abt those 2 thieves who stole a calendar?',
+            'they each got 6 months',
+        ),
+        (
+            '& so the lord said unto john "come forth; & receive eternal life"',
+            'but john came fifth & won a toaster',
+        ),
+        (
+            'why did the monkey fall out the tree?',
+            'cus it was dead',
+        ),
+        (
+            'whats green & has wheels?',
+            'grass. i lied abt the wheels',
+        ),
+        (
+            'went 2 see 2 wifi engineers get married the other day',
+            'the reception was fantastic',
+        ),
+        (
+            'wt did the clock do when it was hungry?',
+            'it went back 4 seconds',
+        ),
+        (
+            'wanna know the last thing my grampa said b4 kicking the bucket?',
+            '"hey, wanna see how far i can kick this bucket?"',
+        ),
+        (
+            'wt did the grape say when it was crushed?',
+            'nothing. it just let out a little wine',
+        ),
+        (
+            'wt did the nut say when it was chasing the other nut?',
+            '"im a cashew"',
+        ),
+        (
+            'wt did the blanket say as it fell off the bed?',
+            '"oh sheet"',
+        ),
+        (
+            'wt do u call monkeys that share an amazon acc?',
+            'prime mates',
+        ),
+        (
+            'ik a chameleon that cant change his colours anymore',
+            'u could say my guy has... a reptile dysfunction',
+        ),
+        (
+            'i snapped a pic of this rlly pretty wheat field the other day',
+            'unfortunately it came out kinda grainy',
+        ),
+        (
+            'u hear abt that new restaurant on the moon?',
+            'apparently the food is gr8, but theres just no atmosphere',
+        ),
+        (
+            'how do u make holy water?',
+            'u boil the hell out of it',
+        ),
+        (
+            'a clown held the door open 4 me the other day',
+            'i thought it was a nice jester',
+        ),
+        (
+            'wt do u call an alligator in a vest?',
+            'an investigator',
+        ),
+        (
+            'wts orange & tastes like an orange?',
+            'an orange',
+        ),
+        (
+            'b4 i was a bot, i used 2 have a job crushing pepsi cans',
+            'it was soda pressing',
+        ),
+        (
+            'i saw a kidnapping 2day',
+            'but i decided not 2 wake him up',
+        ),
+        (
+            'whats brown & sticky?',
+            'a stick',
+        ),
+        (
+            'yo u hear abt that italian chef that died?',
+            'homie pasta way',
+        ),
+        (
+            'man goes 2 the doctor. says "doctor! help! i think im a pair of curtains!"',
+            '2 which the doctor replies "pull urself together"',
+        ),
+        (
+            'wts red & bad 4 ur teeth?',
+            'a brick',
+        ),
+        (
+            'was wondering why the football was getting bigger & bigger',
+            'but then it hit me',
+        ),
+        (
+            'saw an ad 4 a tv today. it read "£1!! volume stuck on full"',
+            'thought 2 myself well goddamn i cant turn that down',
+        ),
+        (
+            'how did the farmer find his wife?',
+            'he tractor down',
+        ),
+        (
+            'yo u hear that rumour abt butter?',
+            '...actually nvm, i shouldnt spread it',
+        ),
+        (
+            'knocked into my bookcase & a book fell on my head',
+            'i can only blame my shelf',
+        ),
+        (
+            'bro i gotta lot of joke abt unemployment',
+            'thing is, none of em work',
+        ),
+        (
+            'whyd the painting go 2 jail?',
+            'cus it was framed',
+        ),
+        (
+            'b careful not 2 spell part backwards',
+            'its a trap',
+        ),
+        (
+            'wts yellow & smells like blue paint?',
+            'yellow paint',
+        ),
+        (
+            'just got banned from b&q, some dickhead wearing orange rocked up & asked me if i "wanted decking"!!',
+            'luckily i got the 1st punch in',
+        ),
+        (
+            'aw nuts, just burnt my hawaiian pizza again',
+            'shoulda put it on aloha setting',
+        ),
+        (
+            'man goes 2 the doctor. says "doctor! u gotta help me! im addicted 2 twitter!"',
+            '2 which the doctor replies "i dont follow u',
+        ),
+        (
+            'wt do u get if u cross a sheep w/ a kangaroo?',
+            'a wooly jumper',
+        ),
+        (
+            'where was the scarecrow when he won the nobel prize?',
+            'he was outstanding in his field',
+        ),
+        (
+            'a sandwich walks in the pub & orders a pint',
+            'barman says "srry, we dont serve food here"',
+        ),
+        (
+            '2 fish in a tank',
+            '1 says 2 the other "do yk how 2 drive this thing??"',
+        ),
+        (
+            'wt do u call a talking turtle w/ 5 heads & a bachelors degree?',
+            'fictional',
+        ),
+        (
+            'why dont pirates have any painkillers?',
+            'cus the parrots eat em all',
+        ),
+        (
+            'wt do u call a cow w/ no wings?',
+            'grounded beef',
+        ),
+        (
+            'my fav bakery shut down yesterday',
+            'word is, the baker kneaded a break',
+        ),
+        (
+            'wt do u call a man w/ a shovel in his head?',
+            'an ambulance?! he has a serious head wound dude',
+        ),
+        (
+            '2 cannibals eating a clown',
+            '1 says 2 the other "does this taste funny 2 u?"',
+        ),
+        (
+            'wts orange & sounds like a parrot?',
+            'a carrot',
+        ),
+        (
+            'when my bro got sent 2 jail he completely flipped, started smearing shit all over the walls & everything',
+            'needless 2 say, we never played monopoly again',
+        ),
+        (
+            'wt do u call a joke that isnt funny?',
+            f'{ctx.message.author.name}',
+        ),
+        (
+            'wt do u call a pencil sharpener that cant sharpen pencils?',
+            'broken',
+        ),
+        (
+            'wt do u call a fish w/ no eyes?',
+            'a fsh',
+        ),
+        (
+            'which cheese is made backwards?',
+            'edam',
+        ),
+        (
+            'wt did the buttcheeks say when they decided 2 unionise?',
+            '"together we can stop this shit"',
+        ),
+        (
+            'roses r red, violets r red, sunflowers r red...',
+            '...HOLY SHIT MY GARDENS ON FIRE',
+        ),
+        (
+            'went 2 the zoo the other day, but the only animal they had was a dog',
+            'it was a shih-tzu',
+        ),
+        (
+            'wt do children & wine have in common?',
+            'theyre both locked in my cellar rn',
+        ),
+        (
+            'having some problems organising a hide & seek tournament',
+            'good players r just so hard 2 find',
+        ),
+        (
+            'wanna know why there r big fences around cemeteries?',
+            'its cus ppl r dying 2 get in',
+        ),
+        (
+            'wts the best thing abt switzerland?',
+            'idk, but its flags a big plus',
+        ),
+        (
+            'howd the medium know wt the ghost got him 4 xmas?',
+            'he felt his presents',
+        ),
+        (
+            'why cant u hear pterodactyls go 2 the bathroom?',
+            'cus the pee is silent',
+        ),
+        (
+            'am considering getting my spine removed',
+            'just feel like its only holding me back',
+        ),
+        (
+            'bruh my fear of elevators is rlly gettin outta hand',
+            'might start takin steps 2 avoid them',
+        ),
+    ]
+    return joke_quotes

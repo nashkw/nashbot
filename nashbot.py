ed83f8ddbc9f59750c8903dffcff9373fc6ce03a
2517003w <nwaugh444@gmail.com>
2022-06-20 13:51:55 +0100

    abstracted response wait loop
    cleaned up kkjoke command by generalising both sets of response handling code into 1 nested function
    
    bot can now be either obedient or disobedient on cancel request

--- nashbot.py
+++ nashbot.py
@@ -65,31 +65,32 @@ async def kkjoke(ctx):
     def check(m):
         return m.channel == ctx.channel and m.author == ctx.message.author
 
+    def clean_msg(m):
+        return m.content.lower().strip().replace('?', '').replace('...', '')
+
+    async def wait_for_response(expected):
+        while True:
+            content = clean_msg(await bot.wait_for("message", check=check))
+            if content in expected:
+                return True  # successful progression
+            elif content in cancel_responses:
+                if random.choice([True, False]):
+                    await read_quote(ctx, random.choice(cancel_obedient))
+                    await read_quote(ctx, ':no_entry_sign: joke cancelled :no_entry_sign:')
+                    return False  # cancel joke
+                else:
+                    await read_quote(ctx, random.choice(cancel_disobedient))
+                    await read_quote(ctx, f'{ctx.message.author.name}: {random.choice(expected)}?')
+                    return True  # successful progression
+            else:
+                await read_quote(ctx, random.choice(await get_unexpected_quotes(random.choice(expected))))
+
     await read_quote(ctx, 'knock knock')
-    while True:
-        m = await bot.wait_for("message", check=check)
-        content = m.content.lower().strip().replace('?', '')
-        if content in step_1_expected:
-            break
-        elif content in cancel_responses:
-            await read_quote(ctx, random.choice(cancel_quotes))
-            break
-        else:
-            await read_quote(ctx, random.choice(await get_unexpected_quotes('whos there')))
-
+    if not await wait_for_response(step_1_expected):
+        return  # cancel joke
     await read_quote(ctx, quotes[0])
-    while True:
-        m = await bot.wait_for("message", check=check)
-        content = m.content.lower().strip().replace('?', '')
-        expected = quotes[0] + ' who'
-        if content == expected:
-            break
-        elif content in cancel_responses:
-            await read_quote(ctx, random.choice(cancel_quotes))
-            break
-        else:
-            await read_quote(ctx, random.choice(await get_unexpected_quotes(expected)))
-
+    if not await wait_for_response([quotes[0] + ' who', ]):
+        return  # cancel joke
     await read_quote(ctx, quotes[1:])
 
 

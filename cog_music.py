dcf3794d4fff288f97c19dfbd931aa7856486139
2517003w <nwaugh444@gmail.com>
2022-06-21 12:34:06 +0100

    fixed music end issues
    fixed issues with how to handle the queue ending
    
    fixed showmusic display

--- cog_music.py
+++ cog_music.py
@@ -17,10 +17,20 @@ class Music(commands.Cog, name='music'):
     def __init__(self, bot):
         self.bot = bot
         self.ydl = YoutubeDL(YDL_OPTS)
-        self.queue = asyncio.Queue()
+        self.queue_sources = asyncio.Queue()
+        self.queue_titles = []
         self.next = asyncio.Event()
         self.nowplaying = ''
         self.looping = False
+
+    async def end_music(self, ctx):
+        ctx.voice_client.stop()
+        self.queue_sources = asyncio.Queue()
+        self.queue_titles = []
+        self.next = asyncio.Event()
+        self.nowplaying = ''
+        self.looping = False
+        await ctx.voice_client.disconnect()
 
     async def music_loop(self, ctx):
         await self.bot.wait_until_ready()
@@ -28,67 +38,68 @@ class Music(commands.Cog, name='music'):
             self.looping = True
             self.next.clear()
             try:
-                mitem = self.queue.get_nowait()
+                source = self.queue_sources.get_nowait()
             except asyncio.QueueEmpty:
+                self.looping = False
+                await self.end_music(ctx)
+                await read_quote(ctx, ':x: end of music queue :x:')
                 break
-
-            ctx.voice_client.play(mitem[1], after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
-            self.nowplaying = mitem[0]
+            ctx.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
+            self.nowplaying = self.queue_titles.pop(0)
             await read_quote(ctx, f':musical_note:  now playing: "{self.nowplaying}" :musical_note:')
             await self.next.wait()
             self.nowplaying = ''
-        await ctx.invoke(self.bot.get_command('stopmusic'))
-        self.looping = False
 
-    @commands.command(name='playmusic', help='tell the bot play music from youtube')
-    async def playmusic(self, ctx,  *, search: str):
+    @commands.command(name='play', help='tell the bot play music from youtube')
+    async def play(self, ctx,  *, search: str):
         await ctx.trigger_typing()
-
-        if ctx.author.voice is None:
+        if not ctx.author.voice:
             await read_quote(ctx, 'TODO join a voice channel 1st bro, i need an audience 4 this kinda thing yk ;)')
             return
-
-        if ctx.voice_client is None:
+        elif not ctx.voice_client:
             await ctx.author.voice.channel.connect()
         else:
             await ctx.voice_client.move_to(ctx.author.voice.channel)
-
         info = self.ydl.extract_info(f"ytsearch:{search}", download=False)['entries'][0]
-        mitem = (info['title'], await discord.FFmpegOpusAudio.from_probe(info['formats'][0]['url'], **FFMPEG_OPTS))
-        await self.queue.put(mitem)
-        await read_quote(ctx, f':white_check_mark: added to music queue: "{mitem[0]}" :white_check_mark: ')
-
+        await self.queue_sources.put(await discord.FFmpegOpusAudio.from_probe(info['formats'][0]['url'], **FFMPEG_OPTS))
+        self.queue_titles.append(info['title'])
+        await read_quote(ctx, f':white_check_mark: added to music queue: "{info["title"]}" :white_check_mark: ')
         if not self.looping:
             await self.music_loop(ctx)
 
-    @commands.command(name='stopmusic', help='tell the bot to stop playing music')
-    async def stopmusic(self, ctx):
-        if ctx.voice_client is None:
+    @commands.command(name='clearmusic', help='tell the bot to clear the music queue')
+    async def clearmusic(self, ctx):
+        if not ctx.voice_client:
             await read_quote(ctx, random.choice(await get_no_music_quotes(ctx)))
         else:
-            self.queue = asyncio.Queue()
-            self.next = asyncio.Event()
-            self.nowplaying = ''
-            await read_quote(ctx, ':no_entry_sign: music stopped :no_entry_sign:')
-            await ctx.voice_client.disconnect()
+            await self.end_music(ctx)
+            await read_quote(ctx, ':x: music queue cleared :x:')
 
-    @commands.command(name='pausemusic', help='tell the bot to pause or unpause music')
+    @commands.command(name='pause', help='tell the bot to pause or unpause music')
     async def pause(self, ctx):
-        if ctx.voice_client is None:
+        if not ctx.voice_client:
             await read_quote(ctx, random.choice(await get_no_music_quotes(ctx)))
         elif ctx.voice_client.is_playing():
             ctx.voice_client.pause()
-            await read_quote(ctx, ':pause_button: music paused :pause_button:')
+            await read_quote(ctx, f':pause_button: paused music: "{self.nowplaying}" :pause_button:')
         elif ctx.voice_client.is_paused():
             ctx.voice_client.resume()
-            await read_quote(ctx, ':arrow_forward: music unpaused :arrow_forward:')
+            await read_quote(ctx, f':arrow_forward: unpaused music: "{self.nowplaying}" :arrow_forward:')
 
-    @commands.command(name='showqueue', help='ask the bot to read out the current music queue')
-    async def showqueue(self, ctx):
-        if self.queue:
-            await read_quote(ctx, 'music queue:')
-            for item in self.queue:
-                await read_quote(ctx, f'> {item}')
+    @commands.command(name='skip', help='tell the bot to skip the current song')
+    async def skip(self, ctx):
+        if not ctx.voice_client:
+            await read_quote(ctx, random.choice(await get_no_music_quotes(ctx)))
+        else:
+            ctx.voice_client.stop()
+            await read_quote(ctx, f':track_next: skipped: "{self.nowplaying}" :track_next: ')
+
+    @commands.command(name='showmusic', help='ask the bot to read out the current music queue')
+    async def showmusic(self, ctx):
+        if self.nowplaying:
+            await read_quote(ctx, ('music queue:', f'> :musical_note: "{self.nowplaying}" :musical_note:'))
+            for item in self.queue_titles:
+                await read_quote(ctx, f'> "{item}"')
         else:
             await read_quote(ctx, 'TODO queue empty')
 

8959d928eb8f2460f1c56747b344dd6c30324e16
2517003w <nwaugh444@gmail.com>
2022-07-04 16:40:07 +0100

    added music loop command
    added loop command
    
    compatible with showqueue, clearqueue and pause, skip and dequeue commands need functionality changed to be intuitive

--- cog_music.py
+++ cog_music.py
@@ -32,6 +32,7 @@ class Music(commands.Cog, name='music'):
         self.bot = bot
         self.q_sources = asyncio.Queue()
         self.q_titles = []
+        self.repeating = None
         self.next = asyncio.Event()
         self.nowplaying = ''
         self.looping = False
@@ -41,6 +42,7 @@ class Music(commands.Cog, name='music'):
         await ctx.voice_client.disconnect()
         self.q_sources = asyncio.Queue()
         self.q_titles = []
+        self.repeating = None
         self.next = asyncio.Event()
         self.nowplaying = ''
         self.looping = False
@@ -50,18 +52,25 @@ class Music(commands.Cog, name='music'):
         self.looping = True
         while not self.bot.is_closed() and self.looping:
             self.next.clear()
-            try:
-                source = self.q_sources.get_nowait()
-            except asyncio.QueueEmpty:
-                self.looping = False
-                await self.end_music(ctx)
-                await read_quote(ctx, ':x: end of music queue :x:')
-                break
+            
+            if self.repeating is None:
+                try:
+                    pre_source = self.q_sources.get_nowait()
+                    self.nowplaying = self.q_titles.pop(0)
+                    await read_official(ctx, f'now playing: "{self.nowplaying}"', 'musical_note')
+                except asyncio.QueueEmpty:
+                    self.looping = False
+                    await self.end_music(ctx)
+                    await read_quote(ctx, ':x: end of music queue :x:')
+                    return
+            else:
+                pre_source = self.repeating
+
+            source = await discord.FFmpegOpusAudio.from_probe(pre_source, **FFMPEG_OPTS)
             ctx.voice_client.play(source, after=lambda _: self.bot.loop.call_soon_threadsafe(self.next.set))
-            self.nowplaying = self.q_titles.pop(0)
-            await read_official(ctx, f'now playing: "{self.nowplaying}"', 'musical_note')
             await self.next.wait()
-            self.nowplaying = ''
+            if self.repeating is not None:
+                self.repeating = pre_source
 
     @commands.command(name='play', help='play music from youtube')
     async def play(self, ctx, *, search: str):
@@ -80,7 +89,7 @@ class Music(commands.Cog, name='music'):
             else:
                 await ctx.voice_client.move_to(ctx.author.voice.channel)
 
-            await self.q_sources.put(await discord.FFmpegOpusAudio.from_probe(info['formats'][0]['url'], **FFMPEG_OPTS))
+            await self.q_sources.put(info['formats'][0]['url'])
             self.q_titles.append(info['title'])
         await read_official(ctx, f'added to music queue: "{info["title"]}"', 'white_check_mark')
         if not self.looping:
@@ -95,6 +104,16 @@ class Music(commands.Cog, name='music'):
         elif ctx.voice_client.is_paused():
             ctx.voice_client.resume()
             await read_official(ctx, f'unpaused music: "{self.nowplaying}"', 'arrow_forward')
+
+    @commands.command(name='loop', aliases=['unloop'], help='set the currently playing song to loop')
+    @is_v_client()
+    async def loop(self, ctx):
+        if self.repeating is not None:
+            self.repeating = None
+            await read_official(ctx, f'stopped looping: "{self.nowplaying}"', 'musical_note')
+        else:
+            self.repeating = True
+            await read_official(ctx, f'now looping: "{self.nowplaying}"', 'repeat')
 
     @commands.command(name='skip', help='skip the currently playing song')
     @is_v_client()
@@ -132,10 +151,11 @@ class Music(commands.Cog, name='music'):
     async def showqueue(self, ctx):
         embed = discord.Embed(title='music queue')
         v = '\n'.join([f'> {index + 1}: "{item}"' for index, item in enumerate(self.q_titles)])
-        embed.add_field(name=f':musical_note: 0: "{self.nowplaying}" :musical_note:', value=v, inline=False)
+        emoji = 'repeat' if self.repeating is not None else 'musical_note'
+        embed.add_field(name=f':{emoji}: 0: "{self.nowplaying}" :{emoji}:', value=v, inline=False)
         await read_embed(ctx.channel, embed)
 
-    async def cog_command_error(self, ctx, error):
+    """async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
             await read_quote(ctx, random.choice(await get_no_music_quotes(ctx)))
         elif isinstance(error, NotInVChannel):
@@ -153,7 +173,7 @@ class Music(commands.Cog, name='music'):
         elif isinstance(error, commands.MissingRequiredArgument) and ctx.command == self.bot.get_command('play'):
             await read_official(ctx, '2 use this cmd u gotta give the name of the song u wanna play bud', 'warning')
         else:
-            await read_official(ctx, f'unknown music error: {str(error).lower()}', 'warning')
+            await read_official(ctx, f'unknown music error: {str(error).lower()}', 'warning')"""
 
 
 def setup(bot):

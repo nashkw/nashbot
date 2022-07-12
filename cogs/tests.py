98e05d4cb5d21514d82388ff54d25c5782f9d4bb
2517003w <nwaugh444@gmail.com>
2022-07-12 09:55:12 +0100

    expanded emojisets tests cmd
    emojisets command now can optionally take the name of an emoji set to test
    
    added error handling for the emojisets command

--- cogs/tests.py
+++ cogs/tests.py
@@ -1,6 +1,7 @@
 # tests.py
-import discord
 
+
+import discord
 from quotes import *
 from resources import *
 
@@ -10,19 +11,36 @@ class Tests(commands.Cog, name='tests'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='emojisets', help='test emoji sets in reactions', hidden=True)
+    @commands.command(name='emojisets', aliases=['esets', 'eset'], help='test emoji sets in reactions', hidden=True)
     @is_nash()
-    async def emojisets(self, ctx):
-        for k, eset in emoji_sets.items():
+    async def emojisets(self, ctx, eset_n: str = None):
+
+        async def test_eset(eset, name):
             egroups = [eset[i:i + 20] for i in range(0, len(eset), 20)]
             for group in egroups:
-                embed = discord.Embed(title=k, description='testing testing')
+                embed = discord.Embed(title=name, description='testing testing')
                 msg = await read_embed(ctx, embed)
                 for emoji in group:
                     await msg.add_reaction(emoji)
 
+        if eset_n:
+            if eset_n in emoji_sets:
+                await test_eset(emoji_sets[eset_n], eset_n)
+            else:
+                raise FailedSearch
+        else:
+            for k, eset in emoji_sets.items():
+                await test_eset(eset, k)
+
     async def error_handling(self, ctx, error):
-        return False
+        if isinstance(error, FailedSearch):
+            if ctx.command == self.bot.get_command('emojisets'):
+                await read_err(ctx, 'uuuh thats not the name of an emoji set, srry man. check for typos maybe ?')
+            else:
+                return False
+        else:
+            return False
+        return True
 
 
 def setup(bot):

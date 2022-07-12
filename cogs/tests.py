9a2e9e01f8f3b490f206f99065f0d965904b873d
2517003w <nwaugh444@gmail.com>
2022-07-12 10:12:48 +0100

    added tests emojitruth cmd
    added tests cog command for examining the true form of emojis
    
    added error handling for emojitruth command

--- cogs/tests.py
+++ cogs/tests.py
@@ -32,10 +32,20 @@ class Tests(commands.Cog, name='tests'):
             for k, eset in emoji_sets.items():
                 await test_eset(eset, k)
 
+    @commands.command(name='emojitruth', aliases=['etruth', 'etrue'], help='test the true form of emojis', hidden=True)
+    @is_nash()
+    async def emojitruth(self, ctx, *, emojis: str):
+        await read_quote(ctx, f"```unaltered: {emojis}\nlist of characters: {list(emojis)}```")
+
     async def error_handling(self, ctx, error):
         if isinstance(error, FailedSearch):
             if ctx.command == self.bot.get_command('emojisets'):
                 await read_err(ctx, 'uuuh thats not the name of an emoji set, srry man. check for typos maybe ?')
+            else:
+                return False
+        elif isinstance(error, commands.MissingRequiredArgument):
+            if ctx.command == self.bot.get_command('emojitruth'):
+                await read_err(ctx, '2 use this cmd u gotta give some emojis to test my man my jester my fool :)')
             else:
                 return False
         else:

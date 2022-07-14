af240d1823431b1b213c010e263551ee106e7730
2517003w <nwaugh444@gmail.com>
2022-07-14 19:47:51 +0100

    accounted for single page paginated embeds
    safe shutdown and footers now account for the possibility of a paginated embed having only one page and thus not being active

--- quotes.py
+++ quotes.py
@@ -30,25 +30,32 @@ class MySource(menus.ListPageSource):
     def __init__(self, pages, title, header=None, footer=None):
         self.title = title
         self.head = header
-        self.foot = '(use the reaction emojis to navigate)' if footer is None else footer
         super().__init__(pages, per_page=1)
+        if footer is None:
+            self.foot = '(use the reaction emojis to navigate)' if self.is_paginating() else footer
+        else:
+            self.foot = footer
 
     async def format_page(self, menu, page):
         if self.head:
             embed = discord.Embed(title=self.title).add_field(name=self.head, value=page)
         else:
             embed = discord.Embed(title=self.title, description=page)
-        return embed.set_footer(text=self.foot)
+        if self.foot:
+            embed.set_footer(text=self.foot)
+        return embed
 
 
 class MyMenuPages(menus.MenuPages):
     def __init__(self, bot_id, source, **kwargs):
-        active_menus.append(self)
+        if source.is_paginating():
+            active_menus.append(self)
         self.bot_id = bot_id
         super().__init__(source, **kwargs)
 
     def reaction_check(self, payload):
         if payload.user_id == self.bot_id and payload.event_type == 'REACTION_REMOVE':
+            print('passed overriden check')
             return True
         return super().reaction_check(payload)
 

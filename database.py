df5bb58cbf9d57d3f4228b57a5dc86751ba7ab5d
2517003w <nwaugh444@gmail.com>
2022-08-04 13:36:41 +0100

    database login hidden
    database login credentials now loaded from .env and will need to be manually added upon setup

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 # database.py
 
 
+from dotenv import get_key
 from motor.motor_asyncio import AsyncIOMotorClient
 
-conn_str = 'mongodb+srv://nashkw:oCN49lf0JjyD1Jgw@nashbot.6c9wf.mongodb.net/?retryWrites=true&w=majority'
+conn_str = f'mongodb+srv://{get_key(".env", "DB_LOGIN")}@nashbot.6c9wf.mongodb.net/?retryWrites=true&w=majority'
 db_client = AsyncIOMotorClient(conn_str)
 r_db = db_client.reminder_db

6c3dd3bfd583b001f794e50339a23ebccc16b177
2517003w <nwaugh444@gmail.com>
2022-08-05 11:38:10 +0100

    added setreminder cmd
    added command for setting reminders that can take either absolute or relative times
    
    help and error handling yet to come

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -1,11 +1,12 @@
 # reminder.py
 
-
-from nashbot import read
+from nashbot import read, errs
 from database import r_db
 from datetime import datetime
 from discord.ext import tasks
+from dateutil.parser import parse, ParserError
 from dpytools.parsers import to_timedelta
+from dpytools.errors import InvalidTimeString
 from discord.ext.commands import Cog, command
 
 
@@ -19,28 +20,35 @@ class Reminder(Cog, name='reminder'):
     @tasks.loop(minutes=1)
     async def reminder_loop(self):
         await self.bot.wait_until_ready()
-        reminders = r_db.reminders.find({'done': False, 'next_time': {'$lte': (now := datetime.now())}})
-        async for reminder in reminders:
-            channel = self.bot.get_channel(reminder['channel_id'])
-            author = await self.bot.fetch_user(reminder['user_id'])
-            await read.official(channel, f'{author.mention} {reminder["content"]}', 'alarm_clock')
-            if (time := reminder['recurrent_time']) is not False:
-                reminder['next_time'] = now + to_timedelta(time)
+        reminders = r_db.reminders.find({'next_time': {'$lte': (now := datetime.now())}})
+        async for r in reminders:
+            channel = self.bot.get_channel(r['channel_id'])
+            author = await self.bot.fetch_user(r['user_id'])
+            await read.official(channel, f'{author.mention} {r["content"]}', 'alarm_clock')
+            if (time := r['recurrent_time']) is not False:
+                await r_db.reminders.update_one({'_id': r['_id']}, {'$set': {'next_time': now + to_timedelta(time)}})
             else:
-                reminder['done'] = True
-            await r_db.reminders.replace_one({'_id': reminder['_id']}, reminder)
+                await r_db.reminders.delete_one({'_id': r['_id']})
 
-    @command(name='reminder', aliases=['remindme', 'remind', 'note2self'], brief='set a reminder in this channel',
+    @command(name='setreminder', aliases=['remindme', 'reminder', 'note2self'], brief='set a reminder in this channel',
              help='TODO',
              usage=['TODO'])
-    async def reminder(self, ctx, time: to_timedelta, *, content):
+    async def setreminder(self, ctx,  content: str, *, time):
+        if len(spaced := time.split(' ')) > 1 and spaced[0] in {'for', 'at', 'in', 'after'}:
+            time = ''.join(spaced[1:])
+        try:
+            try:
+                time = ctx.message.created_at + to_timedelta(time)
+            except InvalidTimeString:
+                time = parse(time, dayfirst=True, fuzzy=True)
+        except ParserError:
+            raise errs.BadArg
         await r_db.reminders.insert_one({
             'user_id': ctx.author.id,
             'channel_id': ctx.channel.id,
-            'next_time': ctx.message.created_at + time,
+            'next_time': time,
             'content': content,
             'recurrent_time': False,
-            'done': False,
         })
         await read.official(ctx, f'reminder set for {time}', 'alarm_clock')
 
